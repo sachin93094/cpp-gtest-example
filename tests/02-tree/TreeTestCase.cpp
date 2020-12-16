5ec96e33cf93592efdce3952e676626a2a810583
SolomidHero <dim.sad23@gmail.com>
2020-12-16 23:54:58 +0300

    creating tree for tree test

--- tests/02-tree/TreeTestCase.cpp
+++ tests/02-tree/TreeTestCase.cpp
@@ -5,24 +5,24 @@
 #include "TreeTestCase.h"
 #include "Tree.h"
 
-TEST(TreeTestCase, PathDoesntExist) {
+TEST_F(TreeTestCase, PathDoesntExist) {
   EXPECT_THROW(GetTree("/not/invalid/path", true), std::invalid_argument);
   EXPECT_THROW(GetTree("/not/invalid/path", false), std::invalid_argument);
 }
 
-TEST(TreeTestCase, Directory) {
-  ASSERT_TRUE(GetTree(".", false).is_dir);
-  EXPECT_NO_THROW(GetTree(".", true));
-  EXPECT_NO_THROW(GetTree(".", false));
+TEST_F(TreeTestCase, Directory) {
+  ASSERT_TRUE(GetTree(testing_folder, false).is_dir);
+  EXPECT_NO_THROW(GetTree(testing_folder, true));
+  EXPECT_NO_THROW(GetTree(testing_folder, false));
 }
 
-TEST(TreeTestCase, NotDirectory) {
+TEST_F(TreeTestCase, NotDirectory) {
   EXPECT_THROW(GetTree("/bin/echo", false), std::invalid_argument);
   EXPECT_THROW(GetTree("/bin/echo", true), std::invalid_argument);
 }
 
-TEST(TreeTestCase, Parameters) {
-  ASSERT_GE(GetTree(".", true).children.size(), 0);
-  ASSERT_TRUE(GetTree(".", true) == GetTree("./././", true));
-  ASSERT_TRUE(GetTree(".", false) == GetTree("./././", false));
+TEST_F(TreeTestCase, Parameters) {
+  ASSERT_GE(GetTree(testing_folder, true).children.size(), 2);
+  ASSERT_TRUE(GetTree(testing_folder, true) == GetTree(testing_folder, true));
+  ASSERT_TRUE(GetTree(testing_folder, false) == GetTree(testing_folder, false));
 }
--- tests/02-tree/TreeTestCase.h
+++ tests/02-tree/TreeTestCase.h
@@ -6,9 +6,33 @@
 #pragma once
 
 #include <gtest/gtest.h>
-#include <boost/filesystem/path.hpp>
+#include <boost/filesystem.hpp>
+
+using boost::filesystem::create_directory;
+using boost::filesystem::exists;
+using boost::filesystem::remove_all;
+using boost::filesystem::ofstream;
 
 class TreeTestCase : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    if (exists(testing_folder)) {
+      throw std::runtime_error("Error: directory " + testing_folder + " already exists.");
+    }
+
+    // create directories
+    create_directory(testing_folder);
+    create_directory(testing_folder + "/1");
+    create_directory(testing_folder + "/2");
+
+    // create file
+    ofstream(testing_folder + "/test_file.txt");
+  }
+
+  virtual void TearDown() {
+    remove_all(testing_folder);
+  }
+
+ public:
+  const std::string testing_folder = "_TESTING_DIRECTORY_";
 };
-
-
