9d10ebd2b8dbae4a94dca7e03d6a69c178cf7844
SolomidHero <dim.sad23@gmail.com>
2020-12-16 03:23:14 +0300

    simple-library tests

--- tests/01-simple-library/AddTestCase.cpp
+++ tests/01-simple-library/AddTestCase.cpp
@@ -5,3 +5,14 @@
 #include "AddTestCase.h"
 #include "Functions.h"
 
+TEST(AddTest, SimpleTest){
+  EXPECT_EQ(Add(2, 2), 4);
+}
+
+TEST_F(AddTestCase, CommonAddTests) {
+  ASSERT_EQ(Add(17, 0), 17);
+  ASSERT_EQ(Add(0, 37), 37);
+  ASSERT_EQ(Add(-1, -1), -2);
+  ASSERT_EQ(Add(3, -1), 2);
+  ASSERT_EQ(Add(-1, 3), 2);
+}
--- tests/01-simple-library/LeapTestCase.cpp
+++ tests/01-simple-library/LeapTestCase.cpp
@@ -6,4 +6,20 @@
 
 #include <Functions.h>
 
+TEST_F(LeapTestCase, IfCasesTest) {
+  ASSERT_FALSE(IsLeap(1));
+  ASSERT_FALSE(IsLeap(17));
+  ASSERT_FALSE(IsLeap(2001));
+  ASSERT_FALSE(IsLeap(1993));
+  ASSERT_FALSE(IsLeap(3100));
 
+  ASSERT_TRUE(IsLeap(400));
+  ASSERT_TRUE(IsLeap(888));
+  ASSERT_TRUE(IsLeap(1996));
+  ASSERT_TRUE(IsLeap(2020));
+}
+
+TEST_F(LeapTestCase, ValueErrorTest) {
+  ASSERT_THROW(IsLeap(0), std::invalid_argument);
+  ASSERT_THROW(IsLeap(-25), std::invalid_argument);
+}
